8bd8c48ef197a7fe3ce15d95493eb2f13d1c67c0
Stuart McHattie <sm49@sanger.ac.uk>
2023-02-24 14:42:28 +0000

    Add mocked render function for imported page

--- test/pages/print_labels/index.spec.js
+++ test/pages/print_labels/index.spec.js
@@ -2,7 +2,7 @@ import { mount } from '@vue/test-utils'
 import Index from '@/pages/print_labels/index'
 
 vi.mock('@/pages/print_labels/destination_plates', () => {
-  return { default: {} }
+  return { default: { render: vi.fn() } }
 })
 
 // these tests are only here because code coverage is at 100%. Ridiculous and I am going to get rid of it.
