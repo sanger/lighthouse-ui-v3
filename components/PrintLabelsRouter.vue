690cdcfdac5602fe5746b0479b0570b8cfb9a55f
Stuart McHattie <sm49@sanger.ac.uk>
2023-02-24 11:46:53 +0000

    Add PrintLabelsRouter component

---
+++ components/PrintLabelsRouter.vue
@@ -0,0 +1,15 @@
+<template>
+  <b-navbar ref="navbar" toggleable="md" type="light" class="bg-white" variant="info">
+    <b-navbar-nav class="ms-auto">
+      <b-nav-item to="/print_labels/destination_plates">Destination Plates</b-nav-item>
+      <b-nav-item to="/print_labels/source_plates">Source Plates</b-nav-item>
+      <b-nav-item to="/print_labels/control_plates">Control Plates</b-nav-item>
+      <b-nav-item to="/print_labels/ad_hoc_plate">Ad Hoc Plates</b-nav-item>
+      <b-nav-item to="/print_labels/reagent_aliquots">Reagent Aliquots</b-nav-item>
+    </b-navbar-nav>
+  </b-navbar>
+</template>
+
+<script>
+export default {}
+</script>
