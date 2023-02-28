da00cdc12e8babda756f40135ec481b7f29a59e5
Stuart McHattie <sm49@sanger.ac.uk>
2023-02-28 11:49:09 +0000

    Add Sentinel Cherrypick page

---
+++ pages/sentinel_cherrypick.vue
@@ -0,0 +1,206 @@
+<template>
+  <b-container>
+    <h1 class="mt-3">Sentinel Cherrypick Batch Creation</h1>
+    <p class="lead">Create a cherrypick submission in Sequencescsape</p>
+    <b-alert
+      ref="alert"
+      v-model="showDismissibleAlert"
+      dismissible
+      :variant="pickListResponse.variant"
+    >
+      {{ pickListResponse.alertMessage
+      }}<a :href="pickListResponse.link">{{ pickListResponse.link }}</a>
+    </b-alert>
+
+    <form class="border">
+      <div class="form-group row">
+        <label for="box-barcode" class="col-sm-4 col-form-label">
+          Please scan Lighthouse box barcode(s)
+          <p class="labwhere-warning">
+            Box and its contents need to be in LabWhere and samples created in Sequencescape, to
+            generate batch
+          </p>
+        </label>
+        <div class="col-sm-8">
+          <b-form-textarea
+            id="box-barcodes"
+            v-model="boxBarcodes"
+            rows="5"
+            max-rows="6"
+          ></b-form-textarea>
+        </div>
+      </div>
+      <div class="form-group row">
+        <div class="col-sm-12">
+          <b-button
+            id="handlePlatesRetrieval"
+            variant="primary"
+            class="float-end"
+            :disabled="platesRetrievalDisabled"
+            @click="getPlates()"
+            >Get plates
+          </b-button>
+          <b-button
+            id="cancelSearch"
+            variant="outline-secondary"
+            class="float-end"
+            @click="cancelSearch()"
+            >Clear
+          </b-button>
+        </div>
+      </div>
+    </form>
+    <br />
+    <h3>Plates to Include in Batch</h3>
+    <b-button
+      id="handleSentinelBatchCreationTop"
+      variant="primary"
+      :disabled="batchCreationDisabled"
+      @click="createBatch()"
+      >Create cherrypick batch
+    </b-button>
+    <b-table
+      id="labware-table"
+      v-model:sort-by="sortBy"
+      v-model:sort-desc="sortDesc"
+      show-empty
+      responsive
+      :items="items"
+      :fields="fields"
+      hover
+      selectable
+      :per-page="perPage"
+      :current-page="currentPage"
+    >
+      <template #cell(selected)="row">
+        <b-form-group>
+          <input v-model="row.item.selected" type="checkbox" />
+        </b-form-group>
+      </template>
+    </b-table>
+    <b-pagination
+      v-model="currentPage"
+      :total-rows="rows"
+      :per-page="perPage"
+      aria-table="labware-table"
+    ></b-pagination>
+    <b-button
+      id="handleSentinelBatchCreationBottom"
+      variant="primary"
+      :disabled="batchCreationDisabled"
+      @click="createBatch()"
+      >Create cherrypick batch
+    </b-button>
+  </b-container>
+</template>
+
+<script>
+import labwhere from '@/modules/labwhere'
+import sequencescape from '@/modules/sequencescape'
+
+export default {
+  data() {
+    return {
+      fields: [
+        { key: 'plateBarcode', label: 'Plate barcode', sortable: true },
+        { key: 'selected', label: 'Include in batch', sortable: true },
+      ],
+      sortBy: 'plateBarcode',
+      sortDesc: true,
+      boxBarcodes: '',
+      showDismissibleAlert: false,
+      alertMessage: '',
+      items: [],
+      pickListResponse: { variant: 'danger' },
+      perPage: 10,
+      currentPage: 1,
+      isCreating: false,
+    }
+  },
+  computed: {
+    platesRetrievalDisabled() {
+      return this.boxBarcodes.length === 0
+    },
+    batchCreationDisabled() {
+      return this.isCreating || this.items.length === 0
+    },
+    rows() {
+      return this.items.length
+    },
+  },
+  methods: {
+    async getPlates() {
+      const boxBarcodesList = this.parseBoxBarcodes(this.boxBarcodes)
+      const resp = await labwhere.getPlatesFromBoxBarcodes(boxBarcodesList)
+      this.handleGetPlatesResponse(resp)
+    },
+    handleGetPlatesResponse(resp) {
+      if (!resp.success) {
+        this.pickListResponse = {
+          alertMessage: 'Could not retrieve plates from LabWhere',
+          variant: 'danger',
+        }
+        this.showDismissibleAlert = true
+      } else {
+        this.items = resp.barcodes.map((barcode) => ({
+          plateBarcode: barcode,
+          selected: true,
+        }))
+      }
+    },
+    async createBatch() {
+      this.isCreating = true
+      const barcodes = this.items
+        .filter((item) => item.selected === true)
+        .map((item) => item.plateBarcode)
+
+      if (barcodes.length > 0) {
+        const resp = await sequencescape.createCherrypickBatch(barcodes)
+        this.handleCreateBatchResponse(resp)
+      } else {
+        this.pickListResponse = {
+          alertMessage: 'Please select one or more plates',
+          variant: 'warning',
+        }
+      }
+      this.isCreating = false
+    },
+    handleCreateBatchResponse(resp) {
+      if (resp.success) {
+        this.pickListResponse = {
+          alertMessage: 'Cherrypicking batch successfully created. Go to this link to view it: ',
+          variant: 'success',
+          link: resp.data.attributes.links[0].url,
+        }
+      } else {
+        this.pickListResponse = {
+          alertMessage: resp.error,
+          variant: 'danger',
+        }
+      }
+      this.showDismissibleAlert = true
+    },
+    cancelSearch() {
+      this.boxBarcodes = ''
+    },
+    parseBoxBarcodes(boxBarcodes) {
+      const listNoBlanks = boxBarcodes.split(/\s+/).filter((b) => b !== '')
+      const listUnique = [...new Set(listNoBlanks)]
+      return listUnique
+    },
+  },
+}
+</script>
+
+<style scoped>
+form {
+  padding: 10px;
+  min-height: 160px;
+}
+.labwhere-warning {
+  color: red;
+}
+button {
+  margin: 5px;
+}
+</style>
