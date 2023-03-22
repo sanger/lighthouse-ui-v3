6b1ee5e0160609ba8995ceaaee439adb62a96737
Stuart McHattie <sm49@sanger.ac.uk>
2023-03-22 10:44:37 +0000

    Remove unused feature flags

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -34,8 +34,6 @@ export default defineNuxtConfig({
       projectId: '',
       studyId: '',
       printers: 'a,b,c',
-      uatActions: 'false',
-      bioseroCherrypick: 'false',
     },
   },
   ssr: false, // Client-side rendering only because of limitations in bootstrap-vue-next
