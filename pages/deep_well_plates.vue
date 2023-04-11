1ba59b7b99b50213db22d5eb5921231a6dd56471
Stuart McHattie <sm49@sanger.ac.uk>
2023-04-11 16:55:59 +0100

    Add deep well plates as a page option

---
+++ pages/deep_well_plates.vue
@@ -0,0 +1,11 @@
+<template>
+  <b-container>
+    <h1 class="mt-3">Deep-Well Plates</h1>
+  </b-container>
+</template>
+
+<script lang="ts">
+export default defineComponent({
+  setup() {},
+})
+</script>
