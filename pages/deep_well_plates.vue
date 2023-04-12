9eaf54c0020c658d1712bcdae599b08824650226
Stuart McHattie <sm49@sanger.ac.uk>
2023-04-12 12:10:08 +0100

    Alter layout of deep-weel plate submission to improve UX

--- pages/deep_well_plates.vue
+++ pages/deep_well_plates.vue
@@ -1,52 +1,46 @@
 <template>
   <b-container>
     <h1 class="mt-3 mb-3">Deep-Well Plates</h1>
-    <b-card no-body>
-      <b-tabs card>
-        <b-tab title="Create">
-          <b-card
-            title="Create Deep-Well Plates"
-            subtitle="Generate externally sourced deep-well plates in Sequencescape so that they may be stamped to shallow-well plates using Limber."
-          >
-            <label for="plate-barcodes" class="mt-3 mb-2">Please scan plate barcode(s): </label>
-            <b-form-textarea
-              id="plate-barcodes"
-              ref="plateBarcodesInput"
-              v-model="plateBarcodes"
-              rows="5"
-              max-rows="6"
-              @keydown.ctrl.enter="submitBarcodes()"
-            />
-            <p class="text-muted text-end tip">
-              <b>Tip:</b> You can also submit with Ctrl + Enter.
-            </p>
-            <div class="float-end">
-              <b-button
-                id="clearBarcodes"
-                variant="outline-secondary"
-                class="me-2"
-                @click="clearBarcodes()"
-              >
-                Clear
-              </b-button>
-              <b-button id="submitBarcodes" variant="primary" @click="submitBarcodes()">
-                Submit
-              </b-button>
-            </div>
-          </b-card>
-        </b-tab>
-        <b-tab title="Results">
-          <b-card
-            title="Create Plate Results"
-            subtitle="Entries below show the result of each create deep-well plate request submitted."
-          >
-            <ul class="mt-3">
-              <li>Submit some create plate requests to see the results here.</li>
-            </ul>
-          </b-card>
-        </b-tab>
-      </b-tabs>
+    <b-card
+      title="Import Deep-Well Plates"
+      subtitle="Add externally sourced deep-well plates to Sequencescape so that they can be stamped to shallow-well plates using Limber."
+    >
+      <label for="plate-barcodes" class="mt-3 mb-2">Please scan plate barcode(s):</label>
+      <b-form-textarea
+        id="plate-barcodes"
+        ref="plateBarcodesInput"
+        v-model="plateBarcodes"
+        rows="5"
+        max-rows="6"
+        @keydown.ctrl.enter="submitBarcodes()"
+      />
+      <p class="text-muted text-end tip"><b>Tip:</b> You can also submit with Ctrl + Enter.</p>
+      <b-button
+        id="clearBarcodes"
+        variant="outline-secondary"
+        class="me-2"
+        @click="clearBarcodes()"
+      >
+        Clear
+      </b-button>
+      <div class="float-end">
+        <b-button id="submitBarcodes" variant="primary" @click="submitBarcodes()">
+          Submit
+        </b-button>
+      </div>
     </b-card>
+    <br />
+    <h3>Import Results</h3>
+    <b-table
+      id="imports-table"
+      class="mt-3"
+      show-empty
+      responsive
+      :items="results"
+      :fields="resultFields"
+      hover
+      head-variant="light"
+    />
   </b-container>
 </template>
 
@@ -56,6 +50,18 @@ export default defineComponent({
   data() {
     return {
       plateBarcodes: '',
+      resultFields: [
+        { key: 'barcode', label: 'Plate Barcode' },
+        { key: 'result', label: 'Status / Errors' },
+        { key: 'ready', label: 'Ready for Limber' },
+      ],
+      results: [
+        {
+          barcode: 'TEST-123456',
+          result: "Error:  Plate with barcode 'TEST-123456' already exists.",
+          ready: 'Yes',
+        },
+      ],
     }
   },
   methods: {
