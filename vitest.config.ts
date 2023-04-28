f127f24c0a3522219f89029a47171f953e62abe7
Stuart McHattie <sm49@sanger.ac.uk>
2023-04-28 14:58:45 +0100

    Don't explicitly import the vue plugin for vitest
    It auto-imports now and actually seems to fail when it's been imported manually.

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,8 +1,6 @@
 import { defineVitestConfig } from 'nuxt-vitest/config'
-import vue from '@vitejs/plugin-vue'
 
 export default defineVitestConfig({
-  plugins: [vue()],
   test: {
     root: '.',
     setupFiles: ['./test/setup.js'],
