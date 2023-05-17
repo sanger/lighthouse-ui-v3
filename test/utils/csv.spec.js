5ea3f6bc13717501d29adea43a696d4d498389b9
Stuart McHattie <sm49@sanger.ac.uk>
2023-05-17 16:21:12 +0100

    Add error handling to CSV file parsing
    and accompanying tests

--- test/utils/csv.spec.js
+++ test/utils/csv.spec.js
@@ -3,11 +3,11 @@ import barcodes from '@/test/data/barcodes'
 import { mockError } from '@/test/constants'
 
 describe('csv', () => {
-  let file
+  let validFile
 
   beforeEach(() => {
     const readFile = fs.readFileSync('./test/data/barcodes.csv', 'ascii')
-    file = new File([readFile], 'barcodes.csv', { type: 'text/csv' })
+    validFile = new File([readFile], 'barcodes.csv', { type: 'text/csv' })
   })
 
   afterEach(() => {
@@ -16,7 +16,7 @@ describe('csv', () => {
 
   describe('#read', () => {
     it('successfully reads a valid file', async () => {
-      const result = await csv.read(file)
+      const result = await csv.read(validFile)
 
       expect(result).toBeDefined()
       expect(result.success).toBeTruthy()
@@ -29,7 +29,7 @@ describe('csv', () => {
         throw mockError
       })
 
-      const result = await csv.read(file)
+      const result = await csv.read(validFile)
 
       expect(result).toBeDefined()
       expect(result.success).toBeFalsy()
@@ -40,10 +40,58 @@ describe('csv', () => {
 
   describe('#parse', () => {
     it('parses a valid CSV file correctly', async () => {
-      const { data } = await csv.read(file)
-      const json = csv.parse(data)
+      const { data } = await csv.read(validFile)
+      const parsed = csv.parse(data)
 
-      expect(json).toEqual(barcodes)
+      expect(parsed).toBeDefined()
+      expect(parsed.success).toBeTruthy()
+      expect(parsed.data).toEqual(barcodes)
+      expect(parsed.error).not.toBeDefined()
+    })
+
+    it.each([
+      {
+        description: 'an empty CSV file',
+        filename: './test/data/bad_csv_no_header.csv',
+        errorMessage: 'Header in column 1 is un-named.',
+      },
+      {
+        description: 'empty header name',
+        filename: './test/data/bad_csv_empty_header.csv',
+        errorMessage: 'Header in column 2 is un-named.',
+      },
+      {
+        description: 'duplicated header names',
+        filename: './test/data/bad_csv_duplicate_headers.csv',
+        errorMessage: 'More than one column has the header "text".',
+      },
+      {
+        description: 'a short line',
+        filename: './test/data/bad_csv_line_3_too_short.csv',
+        errorMessage: 'Line 3 has the wrong number of fields: 1 when there should be 2.',
+      },
+      {
+        description: 'a long line',
+        filename: './test/data/bad_csv_line_4_too_long.csv',
+        errorMessage: 'Line 4 has the wrong number of fields: 3 when there should be 2.',
+      },
+      {
+        description: 'blank rows',
+        filename: './test/data/bad_csv_blank_rows.csv',
+        errorMessage: 'Line 5 contains no data.',
+      },
+    ])('generates correct error for a CSV file with $description', async (inputs) => {
+      const file = fs.readFileSync(inputs.filename, 'ascii')
+      const invalidFile = new File([file], 'barcodes.csv', { type: 'text/csv' })
+      const { data } = await csv.read(invalidFile)
+
+      const parsed = csv.parse(data)
+
+      expect(parsed).toBeDefined()
+      expect(parsed.success).toBeFalsy()
+      expect(parsed.data).not.toBeDefined()
+      expect(parsed.error).toBeDefined()
+      expect(parsed.error.message).toBe(inputs.errorMessage)
     })
   })
 })
--- utils/csv.js
+++ utils/csv.js
@@ -1,3 +1,10 @@
+class CSVParsingError extends Error {
+  constructor(message) {
+    super(message)
+    this.name = 'CSVParsingError'
+  }
+}
+
 const read = async (file) => {
   const reader = new FileReader()
   const promise = new Promise((resolve, reject) => {
@@ -21,6 +28,38 @@ const read = async (file) => {
   }
 }
 
+const validateData = (headers, rows) => {
+  // check that the headers are all populated
+  headers.forEach((header, index) => {
+    if (header.length === 0) {
+      throw new CSVParsingError(`Header in column ${index + 1} is un-named.`)
+    }
+
+    if (headers.filter((fHeader) => header === fHeader).length > 1) {
+      throw new CSVParsingError(`More than one column has the header "${header}".`)
+    }
+  })
+
+  rows.forEach((row, index) => {
+    // create an error if this row is empty and not the last line in the file
+    if (row.length === 0 && index < rows.length - 1) {
+      throw new CSVParsingError(`Line ${index + 2} contains no data.`)
+    }
+
+    const cells = row.split(',')
+
+    // create an error if this row has the wrong number of fields
+    // unless it's empty which can happen for the last line of the file
+    if (row.length > 0 && cells.length !== headers.length) {
+      throw new CSVParsingError(
+        `Line ${index + 2} has the wrong number of fields: ${cells.length} when there should be ${
+          headers.length
+        }.`
+      )
+    }
+  })
+}
+
 const parse = (data) => {
   // split data into rows
   const rows = data.split(/\r?\n/)
@@ -28,15 +67,25 @@ const parse = (data) => {
   // headers are on the first row remove it and turn into an array of headers
   const headers = rows.shift().split(',')
 
-  // return a json object which will be an array of records
-  return rows.map((row) => {
+  try {
+    validateData(headers, rows)
+  } catch (error) {
+    return { success: false, error }
+  }
+
+  const rowData = rows.map((row) => {
     // each record is turned into an object
     // with each item with a key of its header
-    return row.split(',').reduce((record, item, index) => {
+    const cells = row.split(',')
+
+    return cells.reduce((record, item, index) => {
       record[headers[index]] = item
       return record
     }, {})
   })
+
+  // return a json object with an array of records
+  return { success: true, data: rowData }
 }
 
 const CSV = {
@@ -44,4 +93,5 @@ const CSV = {
   parse,
 }
 
+export { CSVParsingError }
 export default CSV
